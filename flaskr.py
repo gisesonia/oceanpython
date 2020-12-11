87f0044eefb896f8e1b002fbfa93ad280492522c
gisesonia <gisesonia@gmail.com>
2020-12-11 18:49:22 +0000

    estrutura do projeto e banco de dados

---
+++ flaskr.py
@@ -0,0 +1,30 @@
+import sqlite3
+from flask import Flask, render_template, request, g, redirect, url_for, \
+    abort, session, flash
+
+#Configurações
+
+DATABASE = './tmp/flaskr.db'
+USERNAME = 'admin'
+PASSWORD = '123456'
+
+#Criando o App
+app = Flask(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def pre_request():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def pos_requisicao():
+    g.bd.close()
+
+@app.route('/')
+def exibir_entradas():
+    sql ="SELECT titulo, texto FROM entradas ORDER BY id DESC"
+    cursor = g.db.execute(sql)
+    entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
+    return render_template('exibir_entradas.html')
