13e08a4865f42b6a26993893507a97083743a7fb
gisesonia <gisesonia@gmail.com>
2020-12-11 19:28:27 +0000

    rotas

--- flaskr.py
+++ flaskr.py
@@ -18,13 +18,44 @@ def conectar_bd():
 def pre_request():
     g.bd = conectar_bd()
 
+
+
 @app.teardown_request
-def pos_requisicao():
+def pos_requisicao(exception):
     g.bd.close()
 
 @app.route('/')
 def exibir_entradas():
     sql ="SELECT titulo, texto FROM entradas ORDER BY id DESC"
-    cursor = g.db.execute(sql)
+    cursor = g.bd.execute(sql)
     entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
     return render_template('exibir_entradas.html')
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entrada():
+    if not session.get('logado'):
+        abort(401)
+    sql = "INSERT INTO entradas (titulo, texto) VALUES (?,?)"
+    g.bd.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.bd.commit()
+    flash('Nova entrada salva com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        if request.form['username'] != USERNAME:
+            erro ='Usuário Inválido'
+        elif request.form['password'] != PASSWORD:
+            erro = 'Senha inválida'
+        else:
+            session['logado'] = True
+            flash('Logado com Sucesso. Bem-Vindo!')
+            return redirect(url_for('exibir_entradas'))
+        return render_template('login.html', erro=erro)
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('O logout foi feito com sucesso')
+    return redirect(url_for('exibir_entradas'))
